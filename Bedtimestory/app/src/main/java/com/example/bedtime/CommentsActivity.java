819cf8fc105bc71e7dc3d8b3d9db9317c26923cb
Kulloveth <kulloveth@gmail.com>
2019-04-26 17:47:31 +0200

    comment activity added

--- Bedtimestory/app/src/main/java/com/example/bedtime/CommentsActivity.java
+++ Bedtimestory/app/src/main/java/com/example/bedtime/CommentsActivity.java
@@ -4,9 +4,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.EditText;
 
 public class CommentsActivity extends AppCompatActivity {
-
+       EditText Comment;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
